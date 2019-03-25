a9f144ce73704de07349bc4ce7b3b1a73c8bdc4d
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-03-25 17:17:10 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,2 +1,10 @@
 let mapView = L.map('mapView').setView([30.18, -90.14], 4)
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(mapView)
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}').addTo(mapView)
+let Louisiana = L.marker([30, -90]).addTo(mapView)
+let Baton Rouge = L.polygon([
+  [30.509, -90.08],
+  [30.503, -90.06],
+  [30.51, -90.047]
+]).addTo(mapView);
+Louisiana.bindPopup('A polygon')
+Baton Rouge.bindPopup('A marker')
