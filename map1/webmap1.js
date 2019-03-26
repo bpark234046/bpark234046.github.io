638c138c1d9a36e259865d5edb9af95a3e095fa8
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-03-25 23:14:16 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,1 +1,11 @@
-let mymap = L.map('mapid').setView([30.18, -90.14], 4)
+let Louisiana = L.map('Louisiana').setView([31, -92], 3)
+L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png').addTo(Louisiana)
+let LSU = L.marker([30.4133, -91.1800]).addTo(Louisiana)
+let polygon = L.polygon([
+[30.511415, -91.174133],
+[30.473722, -91.057859],
+[30.351835, -91.048093],
+[30.388899, -91.209385]
+]).addTo(Louisiana);
+Louisiana.bindPopup('A polygon')
+Louisiana.bindPopup('A marker')
