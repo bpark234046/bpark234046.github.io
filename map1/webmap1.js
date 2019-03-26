d7c0a123153332d0376f4dc2730110445d180f3e
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-03-25 20:02:53 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,4 +1,4 @@
-let map = L.map('mapView').setView([30.18, -90.14], 4)
+let mapView = L.map('mapView').setView([30.18, -90.14], 4)
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
