497570abff15e923fbef01a43cb2fb2d4ccc8dc5
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-03-25 20:01:18 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,5 +1,10 @@
 let map = L.map('mapView').setView([30.18, -90.14], 4)
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}').addTo(mapView)
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mapView);
 let marker = L.marker([30, -90]).addTo(mapView)
 let polygon = L.polygon([
   [30.509, -90.08],
