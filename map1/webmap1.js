0346ca46308bf94d5024b3cb9dc66a0a7c453c0f
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-03-25 20:24:03 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,15 +1,15 @@
-let mapView = L.map('ma').setView([30.18, -90.14], 4)
+let mymap = L.map('mapid').setView([30.18, -90.14], 4)
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'your.mapbox.access.token'
-}).addTo(mapView);
-let marker = L.marker([30, -90]).addTo(mapView)
+}).addTo(mapid);
+let marker = L.marker([30, -90]).addTo(mapid)
 let polygon = L.polygon([
   [30.509, -90.08],
   [30.503, -90.06],
   [30.51, -90.047]
-]).addTo(mapView);
+]).addTo(mapid);
 Louisiana.bindPopup('A polygon')
 Baton Rouge.bindPopup('A marker')
