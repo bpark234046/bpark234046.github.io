c61e07b396bedb6fbb39b65bc9b228c477bababd
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-03-26 00:50:05 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,20 +1,20 @@
-let NewYork = L.map('webmap').setView([31, -92], 8)
+let New_York = L.map('webmap').setView([31, -92], 8)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(Louisiana)
 let  = L.marker([30.4133, -91.1800]).addTo(Louisiana)
-let Baton_Rouge = L.polygon([
+let New_York_City = L.polygon([
 [40.71455, -74.00712],
 [40.72, -74.01],
 [40.71, -74.05],
 [40.81, -74.09],
 [40.65, -74.15],
 [40.7, -74]
-]).addTo(Louisiana);
+]).addTo(New_York);
 var latlngs = [
 [30.53, -91.2],
 [30.67, -91.43],
 [30.04, -91.2]
 ];
-var polyline = L.polyline(latlngs, {color: 'red'}).addTo(Louisiana);
-Baton_Rouge.bindPopup('A polygon');
-LSU.bindPopup('A marker');
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(New_York);
+New_York_City.bindPopup('A polygon');
+Empire_State_Building.bindPopup('A marker');
 polyline.bindPopup('A polyline');
