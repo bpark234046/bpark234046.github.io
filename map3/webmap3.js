5b139d384dcb8a9dbcc22f730240348e2cfd52d4
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-04-06 15:34:09 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,14 +1,28 @@
-let Earth = L.map('webmap2').setView([40, -95], 5);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(Earth);
-L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_ocean_sfc_sst_time/MapServer/WMSServer', {
-  layers: '1',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA'
-}).addTo(Earth)
-L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer', {
-  layers: '1',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA'
-}).addTo(Earth);
+let states = L.map('webmap3').setView([37.5, -95], 4.0);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(states);
+let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson';
+jQuery.getJSON(stateDemographicsUrl, function (data) {
+  let stateStyle = function (feature) {
+    let popu = feature.properties.POPULATION;
+    let stateColor = 'red';
+    if ( popu >= '5555555' ) { stateColor = 'blue' }
+    return {
+      stroke: false,
+      color: stateColor,
+      weight: 1,
+      fillOpacity: 0.2
+  };
+};
+let onEachFeature = function (feature, layer) {
+  let name = feature.properties.STATE_NAME;
+  let pop10 = feature.properties.POP2010;
+  let popu = feature.properties.POPULATION;
+  layer.bindPopup('Total population of ' + name + ' ' + 'is' + ': ' + popu + '<br>Population in 2010 was: ' + pop10 + ' ' )
+};
+  let stateGeojsonOptions = {
+    style: stateStyle,
+    onEachFeature: onEachFeature
+  };
+  L.geoJSON(data, stateGeojsonOptions).addTo(states);
+
+});
