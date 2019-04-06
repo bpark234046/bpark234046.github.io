d0112a5e3a244035eee7259834bb02b6c656e595
bpark234046 <48488254+bpark234046@users.noreply.github.com>
2019-04-06 11:47:31 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,1 +1,14 @@
-
+let Earth = L.map('webmap2').setView([40, -95], 5);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(Earth);
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_ocean_sfc_sst_time/MapServer/WMSServer', {
+  layers: '1',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA'
+}).addTo(Earth)
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer', {
+  layers: '1',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA'
+}).addTo(Earth);
